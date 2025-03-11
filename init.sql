b40d9331f3b31e6130c5653314dda374a0e9a602
GR <immemo1105@gmail.com>
2025-03-10 20:30:52 -0700

    Initial commit, some running files

---
+++ init.sql
@@ -0,0 +1,9 @@
+CREATE TABLE test (
+  id SERIAL PRIMARY KEY,
+  name TEXT
+);
+
+INSERT INTO test (name) VALUES ('Alice'), ('Bob'), ('Charlie');
+
+
+/* Small script for the creation of data to test the container */
